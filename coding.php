57baf7453fd461d4817fb8390cf25241c0d6a03e
KalinduDN <kalindudn@gmail.com>
2017-05-17 19:32:11 +0530

    sa

--- coding.php
+++ coding.php
@@ -1,9 +1,9 @@
 <?php
-	if (isset($_POST['submit'])) {
+	if (isset($_GET['submit'])) {
 		echo "ok";
-		$user = $_POST['name'];
-		$email  = $_POST['email'];
-		$pass = $_POST['pass'];
+		$user = $_GET['name'];
+		$email  = $_GET['email'];
+		$pass = $_GET['pass'];
 		$con = mysqli_connect("sql12.freemysqlhosting.net","sql12174947","RwdgM1aB2Q","sql12174947");
 		$sql = "INSERT INTO User values ('$email','$user','$pass')";
 		if(mysqli_query($con,$sql)){
